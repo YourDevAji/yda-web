7a5739dd55b09047aca638656bc71faba2f408fc
Jibeans <ajibewairekanmi@gmail.com>
2024-11-30 04:36:20 +0200

    Update index.html

--- components/non-popular-pricing-builder/script.js
+++ components/non-popular-pricing-builder/script.js
@@ -6,21 +6,6 @@ const nonPopularPricingWidget = new HtmlWidget();
 
 
 export async function nonPouplarCard(data){
-    //    const data = {
-    //        "title": "ENTERPRISE",
-    //        "description": "Best for large enterprises & teams.",
-    //        "buttonText": "Contact sales",
-    //        "price": "$300",
-    //        "period": "/month",
-    //        "inclusionTitle": "Plan includes:",
-    //        "inclusions": [
-    //            "Unlimited access",
-    //            "24/7 support",
-    //            "Dedicated account manager",
-    //            "Custom solutions"
-    //        ]
-    //    };
-
     const renderedCard = await nonPopularPricingWidget.renderFromFile('/components/non-popular-pricing-builder/index.html', data);
     return renderedCard;
 }
--- components/popular-pricing-builder/script.js
+++ components/popular-pricing-builder/script.js
@@ -6,21 +6,6 @@ const popularPricingWidget = new HtmlWidget();
 
 
 export async function pouplarCard(data){
-    //    const data = {
-    //        "title": "ENTERPRISE",
-    //        "description": "Best for large enterprises & teams.",
-    //        "buttonText": "Contact sales",
-    //        "price": "$300",
-    //        "period": "/month",
-    //        "inclusionTitle": "Plan includes:",
-    //        "inclusions": [
-    //            "Unlimited access",
-    //            "24/7 support",
-    //            "Dedicated account manager",
-    //            "Custom solutions"
-    //        ]
-    //    };
-
     const renderedCard = await popularPricingWidget.renderFromFile('/components/popular-pricing-builder/index.html', data);
     return renderedCard;
 }
